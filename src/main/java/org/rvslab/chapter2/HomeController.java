8a24a8e66c2e4b285883419959e17dad380f4852
jstahovic <jstahovicjr@gmail.com>
2018-10-11 14:27:29 -0400

    Initial updates to the POM and src.

--- src/main/java/org/rvslab/chapter2/HomeController.java
+++ src/main/java/org/rvslab/chapter2/HomeController.java
@@ -1,39 +1,26 @@
 package org.rvslab.chapter2;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Handles requests for the application home page.
- */
-@Controller
+@RestController
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@RequestMapping("/")
+	public Greet sayHello() {
+		return new Greet("Hello World!");
 	}
-	
 }
+
+class Greet { 
+	private String message;
+	public Greet(String message) {
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}	
